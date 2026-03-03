81d7dd01a5b6cb6898c3e3daa73808eb6c04c924
petrskopalik <petr.majetin@seznam.cz>
2026-03-03 22:32:18 +0100

    vEB tree in development

---
+++ Algorithms and data structures/van_emde_boas_tree.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+
+// u=16. Povinné jsou metody MIN, MAX, INSERT, SUCCESSOR, print
+
+double u = 16;
+
+typedef struct node{
+    int n;
+    struct node *summary;
+    struct node *cluster;
+    int min;
+    int max;
+} node;
+
+typedef struct veb_tree{
+    node *root;
+} veb_tree;
+
+veb_tree* make_tree(){
+    veb_tree *new_tree = malloc(sizeof(veb_tree));
+    assert(new_tree);
+
+    return new_tree;
+}
+
+int low(int x){
+    return x % (int)sqrt(u);
+}
+
+int high(int x){
+    return x / (int)sqrt(u);
+}
+
+int index(int x, int y){
+    return x * (int)sqrt(u) + y;
+}
+
+int min(node *n){
+    return n->min;
+}
+
+int max(node *n){
+    return n->max;
+}
+
+void empty_tree_insert(node *n, int x){
+    n->min = x;
+    n->max = x;
+}
+
+void insert(node *n, int x){
+    int new_value = x;
+    if (n->min == NULL){
+        empty_tree_insert(n, new_value);
+    }
+    else {
+        if (x < n->min){
+            int temp = n->min;
+            n->min = new_value;
+            new_value = temp;
+        }
+        if (n->n > 2){
+            if (min(&(n->cluster[high(x)])) == NULL){
+                insert(n->summary, high(x));
+                empty_tree_insert(&(n->cluster[high(x)]), low(x));
+            }
+            else {
+                insert(&(n->cluster[high(x)]), low(x));
+            }
+        }
+        if (new_value > n->max){
+            n->max = new_value;
+        }
+    }
+}
+
+int successor(node *n, int x){
+    if (n->n == 2){
+        if (x == 0 && n->max == 1){
+            return 1;
+        }
+        else {
+            return NULL;
+        }
+    }
+    else if (n->min != NULL && x < n->min){
+        return n->min;
+    }
+    else{
+        int offset;
+        int max_low = max(&(n->cluster[high(x)]));
+        if (max_low != NULL && low(x) < max_low){
+            offset = successor(&(n->cluster[high(x)]), low(x));
+            return index(high(x), offset);
+        }
+        else {
+            int succ_cluster = successor(n->summary, high(x));
+            if (succ_cluster != NULL){
+                return NULL;
+            }
+            else {
+                offset = min(&(n->cluster[succ_cluster]));
+                return index(succ_cluster, offset);
+            }
+        }
+    }
+}
+
+void veb_print(){
+
+}
+
+
+int main(){
+
+
+    return 0;
+}
